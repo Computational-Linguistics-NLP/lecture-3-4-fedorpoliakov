12ff8ab7b6122ce48a37be85afe8a92940760569
Fedor Poliakov <163769621+fedorpoliakov@users.noreply.github.com>
2025-10-14 16:26:16 +0300

    Add files via upload

---
+++ Test_4_1.py
@@ -0,0 +1,13 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> year = int(input())
+1996
+>>> if year % 400 == 0:
+...     print('да')
+... elif year % 4 == 0:
+...     print('да')
+... elif year % 100 == 0:
+...     print('нет')
+... 
+...     
+да
