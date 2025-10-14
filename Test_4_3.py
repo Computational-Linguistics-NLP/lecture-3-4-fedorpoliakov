99c4ea7f1684e81882b0fcf1175b4b41b2b79375
Fedor Poliakov <163769621+fedorpoliakov@users.noreply.github.com>
2025-10-14 16:35:41 +0300

    Add files via upload

---
+++ Test_4_3.py
@@ -0,0 +1,16 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> year = int(input())
+1999
+>>> if year % 4 == 0 and year % 100! == 0:
+...     
+SyntaxError: invalid syntax
+>>> if year % 4 == 0 and year % 100 != 0:
+...     print('da')
+... elif year % 400 == 0:
+...     print('da')
+... else:
+...     print('net')
+... 
+...     
+net
